a2c5944be1d8513cef7ee932debbc7d680e34579
KSKOP <127592672+KSKPAPA@users.noreply.github.com>
2023-06-18 02:31:42 +0530

    Update start.py

--- YukkiMusic/utils/inline/start.py
+++ YukkiMusic/utils/inline/start.py
@@ -12,7 +12,7 @@ from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
 
-from config import GITHUB_REPO, SUPPORT_CHANNEL, SUPPORT_GROUP
+from config import SUPPORT_CHANNEL, SUPPORT_GROUP
 from YukkiMusic import app
 
 
@@ -107,20 +107,11 @@ def private_panel(_, BOT_USERNAME, OWNER: Union[bool, int] = None):
         buttons.append(
             [
                 InlineKeyboardButton(text=_["S_B_7"], user_id=OWNER),
-                InlineKeyboardButton(
-                    text=_["S_B_6"], url=f"{GITHUB_REPO}"
+                InlineKeyboardButton(text=_["S_B_6"], callback_data="ok_viro""
                 ),
             ]
         )
     else:
-        if GITHUB_REPO:
-            buttons.append(
-                [
-                    InlineKeyboardButton(
-                        text=_["S_B_6"], url=f"{GITHUB_REPO}"
-                    ),
-                ]
-            )
         if OWNER:
             buttons.append(
                 [
