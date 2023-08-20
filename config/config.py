1730e3374351ed4afdbc69651eadf786628141e2
KSKOP <127592672+KSKPAPA@users.noreply.github.com>
2023-08-20 23:24:47 +0530

    Update config.py

--- config/config.py
+++ config/config.py
@@ -59,7 +59,7 @@ UPSTREAM_REPO = getenv(
     "UPSTREAM_REPO",
     "https://github.com/Rahulsharma45/YukkiMusicBot",
 )
-UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "master")
+UPSTREAM_BRANCH = getenv("UPSTREAM_BRANCH", "main")
 
 # GIT TOKEN ( if your edited repo is private)
 GIT_TOKEN = getenv("GIT_TOKEN", None)
