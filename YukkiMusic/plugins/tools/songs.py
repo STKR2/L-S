386d8480320b82ae89a98cccd6a5f5f7156e2717
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-07 01:25:53 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -1,67 +1,74 @@
 import os
+import requests
 import yt_dlp
-import datetime
 from pyrogram import filters
-from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
+from pyrogram.types import InlineKeyboardButton, InlineKeyboardMarkup, Message
+from youtube_search import YoutubeSearch
+from FallenMusic import BOT_MENTION, BOT_USERNAME, LOGGER, app
 
-from ... import app
-from strings.filters import command
-from youtubesearchpython import VideosSearch
-
-@app.on_message(command(["بحث", f"يوت"]))
-async def song(client: app, message: Message):
-    aux = await message.reply_text("‹ جاري البحث  ›")
-    
-    if len(message.command) < 2:
-        return await aux.edit("‹ ارسل يوت واسم الملف الصوتي  ›")
-    
+@app.on_message(filters.command(["يوت", "تحميل", "تنزيل", "بحث"]))
+async def song(_, message: Message):
     try:
-        song_name = message.text.split(None, 1)[1]
-        vid = VideosSearch(song_name, limit=1)
-        song_title = vid.result()["result"][0]["title"]
-        song_link = vid.result()["result"][0]["link"]
-        
-        ydl_opts = {
-    "format": "mp3/bestaudio/best",
-    "verbose": True,
-    "geo-bypass": True,
-    "nocheckcertificate": True,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3",
-            "preferredquality": "160",
-        },
-        {
-            "key": "FFmpegNormalize",
-        },
-    ],
-    "outtmpl": f"downloads/{song_title}.mp3",
-    "volume": 256,  # ضبط درجة الصوت
-}
-        
-        await aux.edit("‹ يتم الرفع  ›")
-        
+        await message.delete()
+    except:
+        pass
+    m = await message.reply_text("- يتم البحث الان .")
+
+    query = " ".join(str(i) for i in message.command[1:])
+    ydl_opts = {"format": "bestaudio[ext=m4a]"}
+
+    try:
+        results = YoutubeSearch(query, max_results=5).to_dict()
+        link = f"https://youtube.com{results[0]['url_suffix']}"
+        title = results[0]["title"][:40]
+        thumbnail = results[0]["thumbnails"][0]
+        thumb_name = f"thumb{title}.jpg"
+        thumb = requests.get(thumbnail, allow_redirects=True)
+        open(thumb_name, "wb").write(thumb.content)
+        duration = results[0]["duration"]
+
+    except Exception as ex:
+        LOGGER.error(ex)
+        return await m.edit_text(f"- فشل .\n\n**السبب :** `{ex}`")
+
+    await m.edit_text("- تم الرفع انتضر قليلاً .")
+
+    try:
         with yt_dlp.YoutubeDL(ydl_opts) as ydl:
-            info_dict = ydl.extract_info(song_link, download=True)
-            audio_path = ydl.prepare_filename(info_dict)
-            duration = str(datetime.timedelta(seconds=info_dict['duration']))
+            info_dict = ydl.extract_info(link, download=False)
+            audio_file = ydl.prepare_filename(info_dict)
+            ydl.process_info(info_dict)
 
-        await aux.edit("‹ تم التحميل  ›")
-        
-        # Display message below the audio file and provide a transparent button with the specified link
-        reply_text = f"هذا الملف الصوتي '{song_title}' تم تنزيله\nالمدة: {duration}\nطلب بواسطة: {message.from_user.first_name}"
-        inline_button = InlineKeyboardButton("اونلاين", url="https://t.me/Xl444")
-        markup = InlineKeyboardMarkup([[inline_button]])
+        rep = f"**- الأسم :** [{title[:23]}]({link})\n**- الوقت :** `{duration}`\n**- بواسطة  :** {BOT_MENTION}"
 
-        # Use send_audio method to send the audio file directly
-        await message.reply_audio(audio_path, caption=reply_text, reply_markup=markup)
+        secmul, dur, dur_arr = 1, 0, duration.split(":")
+        for i in range(len(dur_arr) - 1, -1, -1):
+            dur += int(dur_arr[i]) * secmul
+            secmul *= 60
 
-        try:
-            os.remove(audio_path)
-        except:
-            pass
-        
-        await aux.delete()
-    except Exception as e:
-        await aux.edit(f"**Error:** {e}")
+        visit_butt = InlineKeyboardMarkup(
+            [
+                [InlineKeyboardButton(text="- يوتيوب .", url=link)]
+            ]
+        )
+
+        await app.send_audio(
+            chat_id=message.chat.id,
+            audio=audio_file,
+            caption=rep,
+            thumb=thumb_name,
+            title=title,
+            duration=dur,
+            reply_markup=visit_butt,
+        )
+
+        await m.delete()
+
+    except:
+        return await m.edit_text("- فشل الرفع .")
+
+    try:
+        os.remove(audio_file)
+        os.remove(thumb_name)
+    except Exception as ex:
+        LOGGER.error(ex)
