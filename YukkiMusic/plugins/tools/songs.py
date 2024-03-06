5edaa5b7793e0492fb5061b786cdaeff239f9f9f
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-06 23:34:07 +0300

    Update songs.py

--- YukkiMusic/plugins/tools/songs.py
+++ YukkiMusic/plugins/tools/songs.py
@@ -22,20 +22,20 @@ async def song(client: app, message: Message):
         song_link = vid.result()["result"][0]["link"]
         
         ydl_opts = {
-    "format": "mp3/bestaudio/best",
-    "verbose": True,
-    "geo-bypass": True,
-    "nocheckcertificate": True,
-    "postprocessors": [
-        {
-            "key": "FFmpegExtractAudio",
-            "preferredcodec": "mp3"
+            "format": "mp3/bestaudio/best",
+            "verbose": True,
+            "geo-bypass": True,
+            "nocheckcertificate": True,
+            "postprocessors": [
+                {
+                    "key": "FFmpegExtractAudio",
+                    "preferredcodec": "mp3"
+                }
+            ],
+            "outtmpl": f"downloads/{song_title}.mp3",
+            "format": "worstaudio/worst",  # تحديد أدنى جودة ممكنة
+            "audio_quality": "0",  # تحديد جودة الصوت (0 تعني الجودة الأدنى)
         }
-    ],
-    "outtmpl": f"downloads/{song_title}.%(ext)s",
-    "format": "worstaudio/worst",  # تحديد أدنى جودة ممكنة
-    "audio_quality": "9",  # يمكنك ضبط هذه القيمة بين 0 (أدنى) و9 (أعلى)
-}
         
         await aux.edit("‹ يتم الرفع  ›")
         
