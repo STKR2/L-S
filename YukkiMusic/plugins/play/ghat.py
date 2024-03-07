de9871f976f9b79059600e2012ffe17d16485bf7
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-07 22:09:54 +0300

    Update ghat.py

--- YukkiMusic/plugins/play/ghat.py
+++ YukkiMusic/plugins/play/ghat.py
@@ -3,20 +3,13 @@ from YukkiMusic import app
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-async def add_contact_auto(client, user_id: int, first_name: str, username: str = None):
-    try:
-        await client.add_contact(user_id, first_name=first_name, username=username)
-        print(f"تمت إضافة {first_name} إلى جهات الاتصال")
-    except Exception as e:
-        print(f"Error adding contact: {e}")
-
-@app.on_message(filters.command(["اضفني"]))
-async def add_contact_command(_, message: Message):
+@app.on_message(command(["addme",f"ضيفني"]) 
+async def leave_one(client, message):
     try:
         if message.from_user.username:
-            await add_contact_auto(app, message.from_user.id, message.from_user.first_name, username=message.from_user.username)
+            await add_contact(message.from_user.username, message.from_user.first_name)
         else:
-            await add_contact_auto(app, message.from_user.id, message.from_user.first_name)
-        await message.reply_text("تمت إضافتك إلى جهات الاتصال في حساب المساعد")
+            await add_contact(message.from_user.id, message.from_user.first_name)
+        await message.reply_text("تم اضافتك الي جهات الاتصال في الحساب المساعد")
     except Exception as e:
-        await message.reply_text(f"خطأ: {e}")
+        await message.reply_text(f"خطأ : {e}")
