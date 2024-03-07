2b828f11a340f0988aba2e2ccdfabb55f9ef597b
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-07 16:49:27 +0300

    Update small.py

--- YukkiMusic/plugins/play/small.py
+++ YukkiMusic/plugins/play/small.py
@@ -3,22 +3,24 @@ from pyrogram.types import InlineKeyboardMarkup, InlineKeyboardButton, Message
 from strings.filters import command
 from YukkiMusic import app
 import config
-    
+
 @app.on_message(
-    command(["اوامر","الاوامر"])
- )
+    command(["اوامر", "الاوامر"])
+)
 async def mmmezat(client, message):
-        await message.reply_text(f"""مرحبآ بك عزيزي » {message.from_user.mention}في قسم مميزات سورس cr ميوزك\n
-هنا تكتب الاوامر """,
+    await message.reply_text(
+        f"""مرحبآ بك عزيزي » {message.from_user.mention}في قسم مميزات سورس cr ميوزك\n
+        هنا تكتب الاوامر """,
         reply_markup=InlineKeyboardMarkup(
             [
                 [
                     InlineKeyboardButton(
-                        "- المطور .", user_id=config.OWNER_ID),                        
-                 ],[
-                InlineKeyboardButton(
+                        "- المطور .", user_id=config.OWNER_ID[0]),
+                ],
+                [
+                    InlineKeyboardButton(
                         "- مسح .", callback_data="close"),
-               ],
-          ]
+                ],
+            ]
         ),
     )
