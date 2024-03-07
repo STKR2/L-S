37151e7b9e9e75e5d502decf5068c957f10966ba
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-08 01:04:44 +0300

    Update thumbnails.py

--- YukkiMusic/utils/thumbnails.py
+++ YukkiMusic/utils/thumbnails.py
@@ -18,7 +18,7 @@ from PIL import (Image, ImageDraw, ImageEnhance, ImageFilter,
                  ImageFont, ImageOps)
 from youtubesearchpython.__future__ import VideosSearch
 
-from config import MUSIC_BOT_NAME, TELEGRAM_AUDIO_URL
+from config import MUSIC_BOT_NAME, YOUTUBE_IMG_URL
 
 
 def changeImageSize(maxWidth, maxHeight, image):
@@ -148,4 +148,4 @@ async def gen_thumb(videoid):
         background.save(f"cache/{videoid}.png")
         return f"cache/{videoid}.png"
     except Exception:
-        return TELEGRAM_AUDIO_URL
+        return YOUTUBE_IMG_URL
