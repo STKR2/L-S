5bc69b6e79971a881eb3055dca307919606c766d
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-08 00:26:55 +0300

    Update config.py

--- config/config.py
+++ config/config.py
@@ -171,29 +171,29 @@ autoclean = []
 # Images
 START_IMG_URL = getenv("START_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-PING_IMG_URL = getenv("PING_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+PING_IMG_URL = getenv("PING_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+PLAYLIST_IMG_URL = getenv("PLAYLIST_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-GLOBAL_IMG_URL = getenv("GLOBAL_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+GLOBAL_IMG_URL = getenv("GLOBAL_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-STATS_IMG_URL = getenv("STATS_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+STATS_IMG_URL = getenv("STATS_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-TELEGRAM_AUDIO_URL = getenv("TELEGRAM_AUDIO_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+TELEGRAM_AUDIO_URL = getenv("TELEGRAM_AUDIO_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-TELEGRAM_VIDEO_URL = getenv("TELEGRAM_VIDEO_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+TELEGRAM_VIDEO_URL = getenv("TELEGRAM_VIDEO_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-STREAM_IMG_URL = getenv("STREAM_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+STREAM_IMG_URL = getenv("STREAM_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-SOUNCLOUD_IMG_URL = getenv("SOUNCLOUD_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+SOUNCLOUD_IMG_URL = getenv("SOUNCLOUD_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL","https://te.legra.ph/file/402c519808f75bd9b1803.png")
+YOUTUBE_IMG_URL = getenv("YOUTUBE_IMG_URL", "https://te.legra.ph/file/402c519808f75bd9b1803.png")
 
-SPOTIFY_ARTIST_IMG_URL = getenv("SPOTIFY_ARTIST_IMG_URL","https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
+SPOTIFY_ARTIST_IMG_URL = getenv("SPOTIFY_ARTIST_IMG_URL", "https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
 
-SPOTIFY_ALBUM_IMG_URL = getenv("SPOTIFY_ALBUM_IMG_URL","https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
+SPOTIFY_ALBUM_IMG_URL = getenv("SPOTIFY_ALBUM_IMG_URL", "https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
 
-SPOTIFY_PLAYLIST_IMG_URL = getenv("SPOTIFY_PLAYLIST_IMG_URL","https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
+SPOTIFY_PLAYLIST_IMG_URL = getenv("SPOTIFY_PLAYLIST_IMG_URL", "https://graph.org/file/94fa4bb62424ea712eaa2.jpg")
 
 
 def time_to_seconds(time):
