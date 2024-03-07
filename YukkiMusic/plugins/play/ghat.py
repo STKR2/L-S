f3b247d5a9296f127b6ee1388b32ed1e557df78c
Wa <122730917+STKR2@users.noreply.github.com>
2024-03-07 21:56:54 +0300

    Update ghat.py

--- YukkiMusic/plugins/play/ghat.py
+++ YukkiMusic/plugins/play/ghat.py
@@ -9,16 +9,17 @@ async def add_contact(client, user_id: int, first_name: str, username: str = Non
             await client.add_contact(user_id, first_name=first_name, username=username)
         else:
             await client.add_contact(user_id, first_name=first_name)
+        print(f"تمت إضافة {first_name} إلى جهات الاتصال")
     except Exception as e:
         print(f"Error adding contact: {e}")
 
 @app.on_message(filters.command(["اضفني", "ضيفني", "سجلني"]))
-async def add_contact_command(self, message: Message):
+async def add_contact_command(_, message: Message):
     try:
         if message.from_user.username:
-            await add_contact(self, message.from_user.id, message.from_user.first_name, username=message.from_user.username)
+            await add_contact(app, message.from_user.id, message.from_user.first_name, username=message.from_user.username)
         else:
-            await add_contact(self, message.from_user.id, message.from_user.first_name)
+            await add_contact(app, message.from_user.id, message.from_user.first_name)
         await message.reply_text("تمت إضافتك إلى جهات الاتصال في حساب المساعد")
     except Exception as e:
-        await message.reply_text(f"خطأ : {e}")
+        await message.reply_text(f"خطأ: {e}")
